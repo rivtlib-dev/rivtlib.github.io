896afce4418d25ecffc00886f1ea5a8bb810eac5
rholland <rholland@structurelabs.com>
2023-04-26 00:35:46 -0700

    update units.py

--- units.py
+++ units.py
@@ -1,11 +1,9 @@
-"""Unum units for rivtcalc
+"""Unum unit definitions
 
     Add new units at end of file
 """
 
-
 import importlib.util
-import os
 import sys
 from pathlib import Path, PurePath
 
@@ -14,14 +12,13 @@ from rivt.unum.core import Unum
 from rivt.unum.utils import *
 from rivt.unum.utils import uarray
 
-# rivtcalc path information - comment out for more general
-path1 = importlib.util.find_spec("rivt")
-rivpath = Path(path1.origin).parent
+rvpath = importlib.util.find_spec("rivt")
+rivpath = Path(rvpath.origin).parent
 unumpath = Path(rivpath, "unum")
 sys.path.append(str(rivpath))
 sys.path.append(str(unumpath))
-
 # print(sys.path)
+# print(dir())
 
 Unum.set_format(
     mul_separator=" ",
@@ -33,53 +30,66 @@ Unum.set_format(
     auto_norm=True
 )
 
-# print(dir())
-# standard SI units ==== DO NOT MODIFY BETWEEN DOUBLE LINES  ===================
-# temperature conversion is relative degree size, not offset ------------
+# standard SI units ==== DO NOT MODIFY BETWEEN DOUBLE LINES  ============
+#
+# temperature - relative degree size, not offset ------------------------
 K = new_unit("K", 0, "kelvin")
 CELSIUS = new_unit("deg C", K, "degree Celsius")
 FAHR = new_unit("degF", K * 9.0 / 5, "degree Fahrenheit")
+# time
+SEC = S = new_unit("s", 0, "second")
+HZ = new_unit("Hz", 1 / S, "hertz")
+# length
 M = new_unit("m", 0, "meter")
 NM = new_unit("nm", 10**-9 * M, "nanometer")
 UM = new_unit("um", 10**-6 * M, "micrometer")
 MM = new_unit("mm", 10**-3 * M, "millimeter")
 CM = new_unit("cm", 10**-2 * M, "centimeter")
 DM = new_unit("dm", 10**-1 * M, "decimeter")
-SEC = S = new_unit("s", 0, "second")
-A = new_unit("A", 0, "ampere")
-MA = new_unit("mA", 10**-3 * A, "milliampere")
-MOL = new_unit("mol", 0, "mole")
+# mass
 KG = new_unit("kg", 0, "kilogram")
 GRAM = new_unit("gram", 10**-3 * KG, "gram")
 RAD = new_unit("rad", M / M, "radian")
 SR = new_unit("sr", M**2 / M**2, "steradian")
-HZ = new_unit("Hz", 1 / S, "hertz")
+MOL = new_unit("mol", 0, "mole")
+# force
 N = new_unit("N", M * KG / S**2, "newton")
-J = new_unit("J", N * M, "joule")
-W = new_unit("W", J / S, "watt")
+CD = new_unit("cd", 0, "candela")
+LM = new_unit("lm", CD * SR, "lumen")
+LX = new_unit("lx", LM / M**2, "lux")
+# charge
+A = new_unit("A", 0, "ampere")
+MA = new_unit("mA", 10**-3 * A, "milliampere")
 C = new_unit("C", S * A, "coulomb")
 VO = new_unit("V", W / A, "volt")
 F = new_unit("F", C / VO, "farad")
 OHM = new_unit("ohm", VO / A, "ohm")
+J = new_unit("J", N * M, "joule")
+W = new_unit("W", J / S, "watt")
 SIEMENS = new_unit("siemens", A / VO, "siemens")
 WB = new_unit("Wb", VO * SIEMENS, "weber")
 TS = new_unit("TS", WB / M**2, "tesla")
 HENRY = new_unit("H", WB / A, "henry")
-CD = new_unit("cd", 0, "candela")
-LM = new_unit("lm", CD * SR, "lumen")
-LX = new_unit("lx", LM / M**2, "lux")
-# ============  DO NOT MODIFY ABOVE THIS LINE  =================================
-# metric units ----------------------------------------------------------
+# ============  DO NOT MODIFY ABOVE THIS LINE  =============================
+#
+# metric --------------------------------
+#
 G = new_unit("G", 9.80665 * M / S**2, "gravity acceleration")
+# pressure
 PA = new_unit("Pa", N / M**2, "pascal")
 MPA = new_unit("MPa", PA * (10**6), "megapascals")
 KPA = new_unit("KPa", PA * (10**3), "kilopascals")
+# force
 KN = new_unit("KN", N * (10**3), "kilonewton")
 MN = new_unit("MN", N * (10**6), "meganewton")
 KM = new_unit("KM", M * (10**3), "kilometer")
 KNCM = new_unit("KNcM", KN / (M**3), "kilonewton per cubic meter")
-SM = new_unit("sM", (M**2), "square meter")
-# imperial--------------------------------------------------------------
+# area
+SM = new_unit("SM", (M**2), "square meter")
+SCM = new_unit("SCM", (CM**2), "square centimeter")
+#
+# imperial------------------------------
+#
 # length
 IN = new_unit("in", M / 39.370079, "inch")
 FT = new_unit("ft", M / 3.2808399, "foot")

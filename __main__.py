d93d500a53ec70a6e1b8cfd4485f061fc5109db4
rholland <rholland@structurelabs.com>
2023-09-14 20:43:21 -0700

    update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,37 +1,31 @@
-"""intialize if run as script
+"""run as library or script
 
-*rivt* may be run interactively from an IDE or from the command line. In an IDE
-the API calls can be grouped by the standard cell designation #%% and run
-interactively. As e command line within the rivt document folder, **rivt** will
-process the entire file:
+*rivtlib* is designed to be run from within an IDE but may be run from the
+command line:
 
-    python -m rivt
+    python -m rivtlib rivt-filename.py
 
-The program execute the file *rvddnn_file.py* in the folder, where dd is the
-two digit division number and nn the subdivision number. Docs are output as md
-text, PDF and HTML files. See https://rivtDocs.net for the **rivtDocs** user
-manual.
+where *rivt-filename.py* is in the execution folder. See https://rivt-doc.net
+for further details.
 
-This code base uses a last letter convention for variable name types:
+This code base uses a last letter naming convention for indicating variable
+types:
 
 A = array
 B = boolean
+C = class instance
+D = dictionary
+F = float
 I = integer
 L = list
-F = float
 P = path
 S = string
-D = dictionary
-C = class instance
 """
 
-import os
 import sys
-import importlib
-import fnmatch
 from pathlib import Path
 
-__version__ = "0.9.1-beta.1"
+__version__ = "0.8.1-beta.1"
 __author__ = "rholland@structurelabs.com"
 
 
@@ -39,25 +33,21 @@ def cmdlinehelp():
     """command line help"""
 
     print()
-    print("Run command in rivt document folder ")
+    print("Run the following command in a rivt document folder:")
     print()
-    print("                 python -m rivt                             ")
+    print("       python -m rivtlib rivt-filename.py           ")
     print()
-    print("file is rddnn.py - where dd and nn are two digit integers.  ")
+    print("Text output is written to stdout.                   ")
+    print("Other outputs depend on rivt file settings.         ")
     print()
-    print("Output files are written to:      ")
-    print("    current document folder:  README.txt       ")
-    print("    report folder:            rvddnn_file.pdf  ")
-    print("    site folder:              rvdnn_file.html  ")
-    print()
-    print("User manual is at: https://rivtDocs.net        ")
+    print("See User Manual at: https://rivt-doc.net            ")
 
 
 if sys.version_info < (3, 8):
-    sys.exit("rivtCalc requires Python version 3.8 or later")
+    sys.exit("rivtlib requires Python version 3.8 or later")
 
 if __name__ == "__main__":
     try:
-        import rivt.rivtapi
+        import rivtlib.rivtapi
     except:
         cmdlinehelp()

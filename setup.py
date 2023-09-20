ffce319e36b787179a12cab1d60444aad6a8a701
rholland <rholland@structurelabs.com>
2023-09-19 19:16:10 -0700

    build(deps): update pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -1,55 +1,13 @@
-import os
-import site
-site.ENABLE_USER_SITE = os.geteuid() != 0
-
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name="rivtlib",
-    version = "0.0.4",
-    author="rhholland",
-    author_email = "andrewjcarter@gmail.com",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "to the cheese shop a5 pypi.org."),
-    license = "MIT",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['an_example_pypi_project', 'tests'],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],
     packages=[
-        "rivtcalc",
-        "rivtcalc.unum",
-        "rivtcalc.unum.units",
-        "rivtcalc.unum.units.si",
-        "rivtcalc.scripts",
-        "rivtcalc.docs"
+        "rivtlib",
+        "rivtlib.unum",
+        "rivtlib.unum.units",
+        "rivtlib.unum.units.si",
+        "rivtlib.scripts",
+        "rivtlib.docs"
     ],
-    version='0.8.2-beta.0',
-    python_requires='>=3.7',
-    license="MIT",
-    long_description=open("README.rst").read(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "numpy",
-        "sympy",
-        "pandas",
-        "tabulate",
-        "matplotlib",
-        "jupyter",
-        "docutils",
-        "xlrd",
-        "antlr4-python3-runtime>=4.7,<4.8",
-    ],
+    python_requires='>=3.8',
 )

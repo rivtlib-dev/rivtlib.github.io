47f5598fd1c37c1484cad6246f32fa2f59b8c3e7
rholland <rholland@structurelabs.com>
2023-10-25 19:20:41 -0700

    delete 154 files and update 1 file

--- gendocs.cmd
+++ gendocs.cmd
@@ -1,25 +1,24 @@
 echo Y | rmdir docs /S
-pdocs as_markdown .\src\rivtlib\ 
-pdocs as_markdown .\src\rivtlib\cmd_md.py
-pdocs as_markdown .\src\rivtlib\cmd_parse.py
-pdocs as_markdown .\src\rivtlib\cmd_rst.py
-pdocs as_markdown .\src\rivtlib\cmd_utf.py
-pdocs as_markdown .\src\rivtlib\config.py
-pdocs as_markdown .\src\rivtlib\folders.py
-pdocs as_markdown .\src\rivtlib\parse.py
-pdocs as_markdown .\src\rivtlib\report.py
-pdocs as_markdown .\src\rivtlib\rivtapi.py
-pdocs as_markdown .\src\rivtlib\setup.py
-pdocs as_markdown .\src\rivtlib\tag_md.py
-pdocs as_markdown .\src\rivtlib\tag_parse.py
-pdocs as_markdown .\src\rivtlib\tag_rst.py
-pdocs as_markdown .\src\rivtlib\tag_utf.py
-pdocs as_markdown .\src\rivtlib\units.py
-pdocs as_markdown .\src\rivtlib\write_pdf.py
-pdocs as_markdown .\src\rivtlib\write_private.py
-pdocs as_markdown .\src\rivtlib\write_public.py
-pdocs as_markdown .\src\rivtlib\__init__.py
-pdocs as_markdown .\src\rivtlib\__main__.py
+pdoc -d markdown -o  ./src/rivtlib/cmd_md.py
+pdoc -d markdown -o  .\src\rivtlib\cmd_parse.py
+pdoc -d markdown -o  .\src\rivtlib\cmd_rst.py
+pdoc -d markdown -o  .\src\rivtlib\cmd_utf.py
+pdoc -d markdown -o  .\src\rivtlib\config.py
+pdoc -d markdown -o  .\src\rivtlib\folders.py
+pdoc -d markdown -o  .\src\rivtlib\parse.py
+pdoc -d markdown -o  .\src\rivtlib\report.py
+pdoc -d markdown -o  .\src\rivtlib\rivtapi.py
+pdoc -d markdown -o  .\src\rivtlib\setup.py
+pdoc -d markdown -o  .\src\rivtlib\tag_md.py
+pdoc -d markdown -o  .\src\rivtlib\tag_parse.py
+pdoc -d markdown -o  .\src\rivtlib\tag_rst.py
+pdoc -d markdown -o  .\src\rivtlib\tag_utf.py
+pdoc -d markdown -o  .\src\rivtlib\units.py
+pdoc -d markdown -o  .\src\rivtlib\write_pdf.py
+pdoc -d markdown -o  .\src\rivtlib\write_private.py
+pdoc -d markdown -o  .\src\rivtlib\write_public.py
+pdoc -d markdown -o  .\src\rivtlib\__init__.py
+pdoc -d markdown -o  .\src\rivtlib\__main__.py
 sphinx-build . docs
 echo F | xcopy /y CNAME .\docs\CNAME
 echo F | xcopy /y .nojekyll .\docs\.nojekyll

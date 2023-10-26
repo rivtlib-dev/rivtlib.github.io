430d36bc5c2c8c2537d4d22db42ead46a092df09
rholland <rholland@structurelabs.com>
2023-10-26 02:23:30 -0700

    update 255 files

--- conf.py
+++ conf.py
@@ -29,14 +29,16 @@ html_sidebars = {
 }
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static', '_static/img/']
-html_logo = "_static/img/riv-dark8a.png"
+
+# html_logo = "_static/img/riv-dark8a.png"
+# "navbar_start": ["navbar-logo"]
+
 html_theme_options = {
     "show_nav_level": 2,
     "show_toc_level": 3,
     "navigation_depth": 3,
     "navbar_align": "content",
     "navbar_persistent": ["search-button"],
-    "navbar_start": ["navbar-logo"],
     "navbar_center": ["navbar-nav"],
     "navbar_end": ["navbar-icon-links"],
     "primary_sidebar_end": ["indices.html"],

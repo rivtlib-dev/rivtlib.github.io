70846b974878097aaafb0481e8877d5eb537059b
rholland <rholland@structurelabs.com>
2023-10-31 18:00:17 -0700

    update 292 files

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ html_sidebars = {
 }
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static', '_static/img/']
-html_logo = "_static/img/rivtpy3a.png"
+html_logo = "_static/img/rivtpy4a.png"
 
 html_theme_options = {
     "show_nav_level": 2,
@@ -44,7 +44,7 @@ html_theme_options = {
     "footer_end": [],
 
     "logo": {
-        "image_dark": "_static/img/rivtpy3a.png",
+        "image_dark": "_static/img/rivtpy4a.png",
     }
 }
 

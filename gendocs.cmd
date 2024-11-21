218f951b5818fbe10f539cc40dd8366fe79d4441
rholland <rholland@structurelabs.com>
2024-11-21 11:32:28 -0800

    update 262 files and delete 54 files

--- gendocs.cmd
+++ gendocs.cmd
@@ -1,15 +1,15 @@
 echo Y | rmdir docs /S
-pdocs as_markdown -o .\  .\src\rivtlib\cmd_rst.py
-pdocs as_markdown -o .\  .\src\rivtlib\cmd_utf.py
-pdocs as_markdown -o .\  .\src\rivtlib\parse.py
-pdocs as_markdown -o .\  .\src\rivtlib\rivtapi.py
-pdocs as_markdown -o .\  .\src\rivtlib\folders.py
-pdocs as_markdown -o .\  .\src\rivtlib\tag_rst.py
-pdocs as_markdown -o .\  .\src\rivtlib\tag_utf.py
-pdocs as_markdown -o .\  .\src\rivtlib\units.py
-pdocs as_markdown -o .\  .\src\rivtlib\write.py
-pdocs as_markdown -o .\  .\src\rivtlib\__init__.py
-pdocs as_markdown -o .\  .\src\rivtlib\__main__.py
+pdocs as_markdown -o .\  ..\rivtlib\cmd_rst.py
+pdocs as_markdown -o .\  ..\rivtlib\cmd_utf.py
+pdocs as_markdown -o .\  ..\rivtlib\parse.py
+pdocs as_markdown -o .\  ..\rivtlib\rivtapi.py
+pdocs as_markdown -o .\  ..\rivtlib\folders.py
+pdocs as_markdown -o .\  ..\rivtlib\tag_rst.py
+pdocs as_markdown -o .\  ..\rivtlib\tag_utf.py
+pdocs as_markdown -o .\  ..\rivtlib\units.py
+pdocs as_markdown -o .\  ..\rivtlib\write.py
+pdocs as_markdown -o .\  ..\rivtlib\__init__.py
+pdocs as_markdown -o .\  ..\rivtlib\__main__.py
 sphinx-build . docs
 echo F | xcopy /y CNAME .\docs\CNAME
 echo F | xcopy /y .nojekyll .\docs\.nojekyll
--- src/rivtlib/scripts/genpdocs.cmd
+++
@@ -1,1 +0,0 @@
-pdocs as_markdown -o docs .\rv_r.py .\rv_i.py .\rv_v.py .\rv_t.py .\commands.py .\tags.py .\write.py .\units.py .\scan.py .\rvtext

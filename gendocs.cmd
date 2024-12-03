7c8f6053543a44fd238c7d9030782a241ff180e6
rholland <rholland@structurelabs.com>
2024-12-02 23:22:09 -0800

    update 103 files

--- gendocs.cmd
+++ gendocs.cmd
@@ -1,5 +1,6 @@
 echo Y | rmdir docs /S
 echo F | xcopy /y E:\python313\Lib\site-packages\rivtlib\*.* .\src
+pdoc --html --force --output-dir .\ .\src\check.py
 pdoc --html --force --output-dir .\ .\src\parse.py
 pdoc --html --force --output-dir .\ .\src\cmd_rst.py
 pdoc --html --force --output-dir .\ .\src\cmd_utf.py
@@ -9,7 +10,6 @@ pdoc --html --force --output-dir .\ .\src\rivtapi.py
 pdoc --html --force --output-dir .\ .\src\folders.py
 pdoc --html --force --output-dir .\ .\src\__init__.py
 pdoc --html --force --output-dir .\ .\src\__main__.py
-pdoc --html --force --output-dir .\ .\src\check.pyy
 sphinx-build . docs
 echo F | xcopy /y CNAME .\docs\CNAME
 echo F | xcopy /y .nojekyll .\docs\.nojekyll
